3c627605c0db2624c2190cfdb1a460e7c4dc48e2
minji0626 <jo010163@naver.com>
2025-04-20 20:17:53 +0900

    changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import { Route, Routes } from 'react-router-dom';
+
+function Layout(){
+  return(
+    <div>
+
+    </div>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-        <h1 className='text-3xl font-bold underline'>
-          Hello World
-        </h1>
-    </div>
+    <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+    </Routes>
   );
 }
 
